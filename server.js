c832dd786da6b30b17a53d0716af507d62f19568
SergioBumer <sibm.95@outlook.com>
2021-04-22 12:43:47 -0500

    Adición del CORS

--- server.js
+++ server.js
@@ -11,8 +11,15 @@ const artistRoutes = require('./routes/artist');
 const albumRoutes = require('./routes/album');
 const songRoutes = require('./routes/song');
 
-//Cabeceras HTTp
+//Cabeceras HTTP
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS, PUT');
+    res.header('Allow', 'GET, POST, PATCH, DELETE, OPTIONS, PUT');
 
+    next();
+});
 // Rutas Base
 
 server.use('/api', userRoutes);
