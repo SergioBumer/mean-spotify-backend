b69b8404447ff2623051b042cbdfb2c0627c1939
SergioBumer <sibm.95@outlook.com>
2021-04-22 12:44:11 -0500

    Corrección commit previo cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const albumRoutes = require('./routes/album');
 const songRoutes = require('./routes/song');
 
 //Cabeceras HTTP
-app.use((req, res, next) => {
+server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS, PUT');
